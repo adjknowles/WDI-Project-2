1162e8405dba69e30c14041c471d30da56823d09
adjknowles <alastair_knowles@yahoo.co.uk>
2015-11-02 12:54:53 +0000

    Added more instrument seed data

--- findr_musician/db/seeds.rb
+++ findr_musician/db/seeds.rb
@@ -7,7 +7,25 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-Instrument.create!(name: "Guitar")
 Instrument.create!(name: "Piano")
+
+Instrument.create!(name: "Voice")
+
 Instrument.create!(name: "Violin")
+Instrument.create!(name: "Viola")
 Instrument.create!(name: "Cello")
+Instrument.create!(name: "Double Bass")
+
+Instrument.create!(name: "Oboe")
+Instrument.create!(name: "Flute")
+Instrument.create!(name: "Clarinet")
+Instrument.create!(name: "Bassoon")
+
+Instrument.create!(name: "Trumpet")
+Instrument.create!(name: "Trombone")
+Instrument.create!(name: "French Horn")
+Instrument.create!(name: "Tuba")
+
+Instrument.create!(name: "Guitar")
+
+Instrument.create!(name: "Drums")

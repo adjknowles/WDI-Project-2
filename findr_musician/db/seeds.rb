d1e55dc855039e916a9f6bdfc574443d0c31d767
adjknowles <alastair_knowles@yahoo.co.uk>
2015-11-03 10:19:46 +0000

    Added fields to user show.html.erb

--- findr_musician/db/seeds.rb
+++ findr_musician/db/seeds.rb
@@ -6,29 +6,32 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create!(name: "Bob Eddis", email: "bob@bob.com", role: "Student", password: "password", address: "London", contact_number: "02057337956", image: "https://rockyourcareer.files.wordpress.com/2012/11/george_blomgren_med-pic.jpg")
+u1 = User.create!(name: "Bob Eddis", email: "bob@bob.com", role: "Student", password: "password", address: "London", contact_number: "02057337956", image: "https://rockyourcareer.files.wordpress.com/2012/11/george_blomgren_med-pic.jpg")
 
-User.create!(name: "Emily Smith", email: "emily@emily.com", role: "Teacher", password: "password", address: "London", contact_number: "02057337356", image: "http://podcast.farnoosh.tv/wp-content/uploads/2015/06/laura-vanderkam-profile.jpg")
+u2 = User.create!(name: "Emily Smith", email: "emily@emily.com", role: "Teacher", password: "password", address: "London", contact_number: "02057337356", image: "http://podcast.farnoosh.tv/wp-content/uploads/2015/06/laura-vanderkam-profile.jpg")
 
-Instrument.create!(name: "Piano")
+i1 = Instrument.create!(name: "Piano")
 
-Instrument.create!(name: "Voice")
+i2 = Instrument.create!(name: "Voice")
 
-Instrument.create!(name: "Violin")
-Instrument.create!(name: "Viola")
-Instrument.create!(name: "Cello")
-Instrument.create!(name: "Double Bass")
+i3 = Instrument.create!(name: "Violin")
+i4 = Instrument.create!(name: "Viola")
+i5 = Instrument.create!(name: "Cello")
+i6 = Instrument.create!(name: "Double Bass")
 
-Instrument.create!(name: "Oboe")
-Instrument.create!(name: "Flute")
-Instrument.create!(name: "Clarinet")
-Instrument.create!(name: "Bassoon")
+i7 = Instrument.create!(name: "Oboe")
+i8 = Instrument.create!(name: "Flute")
+i9 = Instrument.create!(name: "Clarinet")
+i10 = Instrument.create!(name: "Bassoon")
 
-Instrument.create!(name: "Trumpet")
-Instrument.create!(name: "Trombone")
-Instrument.create!(name: "French Horn")
-Instrument.create!(name: "Tuba")
+i11 = Instrument.create!(name: "Trumpet")
+i12 = Instrument.create!(name: "Trombone")
+i13 = Instrument.create!(name: "French Horn")
+i14 = Instrument.create!(name: "Tuba")
 
-Instrument.create!(name: "Guitar")
+i15 = Instrument.create!(name: "Guitar")
 
-Instrument.create!(name: "Drums")
+i16 = Instrument.create!(name: "Drums")
+
+u2.instruments << i1
+u2.save
